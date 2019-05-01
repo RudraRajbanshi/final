32cd4f18ef71fa6ce5c8d50c9e8e5f376e8df516
RudraRajbanshi <rudra.raj.716@gmail.com>
2019-05-01 09:48:11 +0545

    login form and registration form designed using fragments. Also used shared preferences for storing and retrieving data

---
+++ app/src/main/java/com/onlineclothingshoppingapp/DashboardActivity.java
@@ -0,0 +1,28 @@
+package com.onlineclothingshoppingapp;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class DashboardActivity extends AppCompatActivity {
+    private Button btnAdditem;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+
+        btnAdditem = findViewById(R.id.btnAdditem);
+
+        btnAdditem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DashboardActivity.this,AdditemActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
